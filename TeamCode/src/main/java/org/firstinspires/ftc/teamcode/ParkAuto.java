dfcf63429882e13839db61e114873e008280303b
Software Poet <baronz@usa.net>
2022-11-10 12:33:14 -0500

    Autonomous framework, work in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkAuto.java
@@ -145,7 +145,7 @@ public class ParkAuto extends LinearOpMode {
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
         //1 tick == 6.7 inches roughly, this has been accounted for in the code (input inches)
-        encoderDrive(DRIVE_SPEED, 47, 47, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(DRIVE_SPEED, 30, 30, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         //encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
